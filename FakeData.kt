7a5d0e11060143aef8fd4ed573af2d89f2831f42
Mobin <77391567+M-O-B-I-N@users.noreply.github.com>
2023-10-20 11:21:58 +0330

    Add files via upload

---
+++ FakeData.kt
@@ -0,0 +1,71 @@
+package mobin.shabanifar.composetraining10
+
+data class FakeData(
+    val imageUrl: Int,
+    val id: String,
+    val price: String,
+    val string3: String
+)
+
+val fakeList: List<FakeData> = listOf(
+    FakeData(
+        imageUrl = R.drawable.ic_launcher_foreground,
+        id = "#120",
+        price = "5.249 ETH",
+        string3 = "sed do eiusmod tempor"
+    ),
+    FakeData(
+        imageUrl = R.drawable.ic_launcher_background,
+        id = "#45452",
+        price = "978.518 ETH",
+        string3 = "laboris nisi ut aliquip ex ea commodo consequat"
+    ),
+    FakeData(
+        imageUrl = R.drawable.ic_launcher_foreground,
+        id = "#54330",
+        price = "435.768 ETH",
+        string3 = "dolore eu fugiat nulla pariatur"
+    ),
+    FakeData(
+        imageUrl = R.drawable.ic_launcher_foreground,
+        id = "#120",
+        price = "5.249 ETH",
+        string3 = "sed do eiusmod tempor"
+    ),
+    FakeData(
+        imageUrl = R.drawable.ic_launcher_background,
+        id = "#45452",
+        price = "978.518 ETH",
+        string3 = "laboris nisi ut aliquip ex ea commodo consequat"
+    ),
+    FakeData(
+        imageUrl = R.drawable.ic_launcher_foreground,
+        id = "#54330",
+        price = "435.768 ETH",
+        string3 = "dolore eu fugiat nulla pariatur"
+    ),
+    FakeData(
+        imageUrl = R.drawable.ic_launcher_foreground,
+        id = "#120",
+        price = "5.249 ETH",
+        string3 = "sed do eiusmod tempor"
+    ),
+    FakeData(
+        imageUrl = R.drawable.ic_launcher_background,
+        id = "#45452",
+        price = "978.518 ETH",
+        string3 = "laboris nisi ut aliquip ex ea commodo consequat"
+    ),
+    FakeData(
+        imageUrl = R.drawable.ic_launcher_foreground,
+        id = "#54330",
+        price = "435.768 ETH",
+        string3 = "dolore eu fugiat nulla pariatur"
+    ),
+    FakeData(
+        imageUrl = R.drawable.ic_launcher_foreground,
+        id = "#54330",
+        price = "435.768 ETH",
+        string3 = "dolore eu fugiat nulla pariatur"
+    )
+)
